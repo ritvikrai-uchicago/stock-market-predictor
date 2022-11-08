ce089daf321cfe813e14c171ddca95fb70979a5f
ritvikrai-uchicago <114976622+ritvikrai-uchicago@users.noreply.github.com>
2022-11-08 07:26:45 -0600

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,13 @@
-impo
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+dataset_train = pd.read_csv("Google_Stock_Price_Train.csv")
+dataset_train.head()
+
+training_set = dataset_train.iloc[:, 1:2].values
+
+print(training_set)
+print(training_set.shape)
